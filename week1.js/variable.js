3db6e540a028591ac1178db78e4ff71ab7cdc979
princedev3 <114477979+princedev3@users.noreply.github.com>
2022-10-14 11:03:25 +0300

    Add files via upload

---
+++ week1.js/variable.js
@@ -0,0 +1,68 @@
+//DAY 1
+
+//comments can make code readable
+
+//Welcome to 30DaysOfJavaScript
+
+/*
+comments can make code readable, easy to reuse and informative
+
+*/
+
+//
+
+const variable = Boolean;
+const variable1 = undefined;
+const variable2 = "string";
+const variable3 = null;
+const variable4= 123
+console.log(variable)
+console.log(variable1)
+console.log(variable2)
+console.log(variable3)
+console.log(variable4)
+
+
+
+console.log(typeof variable)
+console.log(typeof variable1)
+console.log(typeof variable2)
+console.log(typeof variable3)
+console.log(typeof variable4)
+
+
+const valueVariable1 = "string"
+const valueVariable2 = 123
+const valueVariable3 = null
+const valueVariable4 = true
+
+
+const firstName = "prince"
+const lasttName = "fejiro"
+const myAge= 28
+const myCountry= "Nigeria"
+const maritalStatus = "single"
+
+
+
+let num;
+let Mycountry;
+let myMarriage;
+let Name;
+
+
+
+let aGe = 25
+
+console.log("i am "+aGe + " years old")
+
+let yourAge = 30
+
+console.log("i am "+ yourAge + " years old")
+
+
+
+
+
+
+
